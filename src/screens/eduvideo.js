90153eaa6626c76b6752bb6a42c54449b7ced103
smulionOong <1224police@likelion.org>
2020-12-06 23:30:02 +0900

    비디오 띄우기

--- src/screens/eduvideo.js
+++ src/screens/eduvideo.js
@@ -1,55 +1,37 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import { WebView } from 'react-native-webview';
+import { SafeAreaView } from 'react-navigation';
 
 
-const eduvideo = (props) => {
+const eduvideo = () => {
   return (
-    <View>
-      <Text>교육 매뉴얼 - 동영상 삽입</Text>
-      {/* <WebView
-        ref={this.webViewRef}
-        allowsFullscreenVideo
-        useWebKit
-        onLoad={this.webViewLoaded}
-        allowsInlineMediaPlayback
-        mediaPlaybackRequiresUserAction
-        javaScriptEnabled
-        scrollEnabled={false}
-        //https://www.youtube.com/watch?v=fgGg5D66t1w
-        source={{ uri: 'https://www.youtube.com/embed/fgGg5D66t1w' }}
-      /> */}
-      {/* <WebView
-        source={{ uri: 'https://www.youtube.com/embed/mXRfApkMYZU' }}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}   
-      /> */}
-      {/* <WebView
-        style={{flex:1}}
-        javaScriptEnabled={true}
-        source={{uri: 'https://www.youtube.com/embed/fgGg5D66t1w?rel=0&autoplay=0&showinfo=0&controls=0&fullscreen=1'}}
-      /> */}
-      <View style={{
-            height: 500,
-            borderWidth:1,
-            borderColor: 'black'
-          }}>
-        <WebView 
-          ref={this.webViewRef}
-          allowsFullscreenVideo
-          useWebKit
-          onLoad={this.webViewLoaded}
-          allowsInlineMediaPlayback
-          mediaPlaybackRequiresUserAction
-          javaScriptEnabled
-          scrollEnabled={false}
-          source={{uri:'https://www.youtube.com/embed/fgGg5D66t1w'}}
-        />
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.videostyle}>
+          <WebView 
+            ref={this.webViewRef}
+            allowsFullscreenVideo
+            useWebKit
+            onLoad={this.webViewLoaded}
+            allowsInlineMediaPlayback
+            mediaPlaybackRequiresUserAction
+            javaScriptEnabled
+            domStorageEnabled
+            scrollEnabled={false}
+            source={{uri:'https://www.youtube.com/embed/fgGg5D66t1w'}}
+          />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  videostyle: {
+    justifyContent: 'center',
+    height: "50%",
+    borderWidth:1,
+    borderColor: 'black'
+  }
+});
 
 export default eduvideo;
